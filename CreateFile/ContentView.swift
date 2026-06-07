12ab42f06fd3f1005f087b001479d7e93ed25534
linruisheng <1919139312@qq.com>
2026-06-07 11:17:42 +0800

    Initial Commit

---
+++ CreateFile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CreateFile
+//
+//  Created by 林瑞生 on 2026/6/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CreateFile/CreateFileApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateFileApp.swift
+//  CreateFile
+//
+//  Created by 林瑞生 on 2026/6/7.
+//
+
+import SwiftUI
+
+@main
+struct CreateFileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
